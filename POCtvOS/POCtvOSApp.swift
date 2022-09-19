400abbf922ef8135608979d20f5ff039beb9712c
Carlos Sanchez <carlos.sanchez@ymedialabs.com>
2022-09-19 12:57:02 -0500

    Initial Commit

---
+++ POCtvOS/POCtvOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCtvOSApp.swift
+//  POCtvOS
+//
+//  Created by Carlos Uribe on 19/09/22.
+//
+
+import SwiftUI
+
+@main
+struct POCtvOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
